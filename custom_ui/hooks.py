4489c11f592fdf7332e4c4451af1bd8f29d0e439
nithin5995 <nithinlogu1234@gmail.com>
2026-04-28 19:13:12 +0530

    ui change

--- custom_ui/hooks.py
+++ custom_ui/hooks.py
@@ -4,9 +4,9 @@ app_publisher = "custom app"
 app_description = "custom app"
 app_email = "itdevelopment@liquiconnect.in"
 app_license = "mit"
-app_include_css = ["/assets/custom_ui/css/fleet_dashboard.css"]   # all desk pages
-app_include_js  = ["/assets/custom_ui/js/fleet_dashboard.js"]
-web_include_css = ["/assets/custom_ui/css/fleet_dashboard.css"]   # www pages
+app_include_css = ["/assets/custom_ui/css/ubs_theme.css"]   # all desk pages
+app_include_js  = ["/assets/custom_ui/js/ubs_home.js"]
+web_include_css = ["/assets/custom_ui/css/ubs_theme.css"]   # www pages
 
 # Apps
 # ------------------
